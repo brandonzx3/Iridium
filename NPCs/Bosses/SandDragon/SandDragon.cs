e39e241a0255d74bd7ebd41ef315fff11b95e1df
Brandonzx3 <btherr2003@gmail.com>
2020-09-06 18:10:10 -0500

    sand dragon soots fire

--- NPCs/Bosses/SandDragon/SandDragon.cs
+++ NPCs/Bosses/SandDragon/SandDragon.cs
@@ -10,6 +10,8 @@ namespace iridium.NPCs.Bosses.SandDragon {
         //AI
         int frame;
         int waitTime;
+        Boolean phase2 = false;
+        int attackTimer = 0;
 
         public override void SetStaticDefaults() {
             Main.npcFrameCount[npc.type] = 1;
@@ -17,8 +19,8 @@ namespace iridium.NPCs.Bosses.SandDragon {
 
         public override void SetDefaults() {
             npc.aiStyle = -1;
-            npc.lifeMax = 3000;
-            npc.damage = 40;
+            npc.lifeMax = 6000;
+            npc.damage = 30;
             npc.defense = 20;
             npc.knockBackResist = 0f;
             npc.width = 250;
@@ -31,7 +33,7 @@ namespace iridium.NPCs.Bosses.SandDragon {
 			npc.HitSound = SoundID.NPCHit1;
 			npc.DeathSound = SoundID.NPCDeath1;
 			npc.buffImmune[24] = true;
-			music = MusicID.Boss5;
+			music = MusicID.PirateInvasion;
         }
 
         public override void AI() {
@@ -43,6 +45,14 @@ namespace iridium.NPCs.Bosses.SandDragon {
 
             npc.netAlways = true;
 
+            if(npc.life <= npc.lifeMax / 2) {
+                npc.ai[0] = -1;
+                npc.ai[1]++;
+            } else {
+                npc.ai[0]++;
+            }
+
+
             if(player.dead || !player.active) {
                 npc.TargetClosest(false);
                 npc.direction = 1;
@@ -53,34 +63,55 @@ namespace iridium.NPCs.Bosses.SandDragon {
                 }
             }
             
-            npc.ai[0]++;
             float distance = Vector2.Distance(target, npc.Center);
-            if(npc.ai[0] < 100) {
-                npc.damage = 40;
-                frame = 0;
-                npc.velocity.Y = -0.1f;
-                if(npc.position.X > player.position.X) {
-                        npc.spriteDirection = 1;
-                        direction = player.Center - npc.Center;
-                        rotation = (float) Math.Atan2(direction.Y, direction.X) - (float) Math.PI;
-                    } else {
+            if(npc.ai[0] != -1) {
+                if(npc.ai[0] < 100) {
+                    npc.damage = 30;
+                    frame = 0;
+                    npc.velocity.Y = -0.1f;
+                    if(npc.position.X > player.position.X) {
+                            npc.spriteDirection = 1;
+                            direction = player.Center - npc.Center;
+                            rotation = (float) Math.Atan2(direction.Y, direction.X) - (float) Math.PI;
+                        } else {
+                            npc.spriteDirection = 0;
+                            direction = player.Center - npc.Center;
+                            rotation = (float) Math.Atan2(direction.Y, direction.X);
+                        }
+                        npc.rotation = rotation;
+                    npc.netUpdate = true;
+                } else if(npc.ai[0] >= 100 && npc.ai[0] < 650) {
+                    if(npc.ai[0] % 80 == 0) {
                         npc.spriteDirection = 0;
-                        direction = player.Center - npc.Center;
-                        rotation = (float) Math.Atan2(direction.Y, direction.X);
+                        npc.damage = 40;
+                        frame = 1;
+                        float speed = 10f;
+                        Vector2 vector = new Vector2(npc.position.X + (float) npc.width * 0.5f, npc.position.Y + (float) npc.height * 0.5f);
+                        float x = player.position.X + (float) (player.width / 2) - vector.X;
+                        float y = player.position.Y + (float) (player.height / 2) - vector.Y;
+                        float distance2 = (float)Math.Sqrt(x * x + y * y);
+                        float factor = speed / distance2;
+                        if(npc.position.X > player.position.X) {
+                            npc.spriteDirection = 1;
+                            direction = player.Center - npc.Center;
+                            rotation = (float) Math.Atan2(direction.Y, direction.X) - (float) Math.PI;
+                        } else {
+                            npc.spriteDirection = 0;
+                            direction = player.Center - npc.Center;
+                            rotation = (float) Math.Atan2(direction.Y, direction.X);
+                        }
+                        npc.rotation = rotation;
+                        npc.velocity.X = x * factor;
+                        npc.velocity.Y = y * factor;
+                        Main.PlaySound(SoundID.Roar);
+                    } else {
+                        frame = 2;
                     }
-                    npc.rotation = rotation;
-                npc.netUpdate = true;
-            } else if(npc.ai[0] >= 100 && npc.ai[0] < 650) {
-                if(npc.ai[0] % 50 == 0) {
-                    npc.spriteDirection = 0;
-                    npc.damage = 80;
-                    frame = 1;
-                    float speed = 10f;
-                    Vector2 vector = new Vector2(npc.position.X + (float) npc.width * 0.5f, npc.position.Y + (float) npc.height * 0.5f);
-                    float x = player.position.X + (float) (player.width / 2) - vector.X;
-                    float y = player.position.Y + (float) (player.height / 2) - vector.Y;
-                    float distance2 = (float)Math.Sqrt(x * x + y * y);
-                    float factor = speed / distance2;
+                    npc.netUpdate = true;
+                } else if(npc.ai[0] >= 650 && npc.ai[0] < 1000) {
+                    frame = 2;
+                    npc.damage = 30;
+
                     if(npc.position.X > player.position.X) {
                         npc.spriteDirection = 1;
                         direction = player.Center - npc.Center;
@@ -91,30 +122,25 @@ namespace iridium.NPCs.Bosses.SandDragon {
                         rotation = (float) Math.Atan2(direction.Y, direction.X);
                     }
                     npc.rotation = rotation;
-                    npc.velocity.X = x * factor;
-                    npc.velocity.Y = y * factor;
-                } else {
-                    frame = 2;
+                    MoveTwards(npc, target, 5f, 0f);
+                    Vector2 shootPos = npc.Center;
+                    float accuracy = 5f * (npc.life / npc.lifeMax);
+                    Vector2 shootVel = target - shootPos + new Vector2(Main.rand.NextFloat(-accuracy, accuracy), Main.rand.NextFloat(-accuracy, accuracy));
+                    shootVel.Normalize();
+                    shootVel *= 7.5f;
+                    Projectile.NewProjectile(shootPos.X + (float) (-100 * npc.direction) + (float) Main.rand.Next(-40, 41), shootPos.Y - (float) Main.rand.Next(-50, 40), shootVel.X, shootVel.Y, npc.damage, 500, 5f);
+                    npc.netUpdate = true;
+                } else if(npc.ai[0] > 1000) {
+                    npc.ai[0] = 100;
                 }
-                npc.netUpdate = true;
-            } else if(npc.ai[0] >= 650 && npc.ai[0] < 1000) {
-                frame = 2;
-                npc.damage = 40;
-
-                if(npc.position.X > player.position.X) {
-                    npc.spriteDirection = 1;
-                    direction = player.Center - npc.Center;
-                    rotation = (float) Math.Atan2(direction.Y, direction.X) - (float) Math.PI;
-                } else {
-                    npc.spriteDirection = 0;
-                    direction = player.Center - npc.Center;
-                    rotation = (float) Math.Atan2(direction.Y, direction.X);
+            } else if(npc.ai[1] != -1) {
+                //phase 2
+                if(npc.ai[1] < 70) {
+                    if(!phase2) {
+                        Main.PlaySound(SoundID.Roar);
+                        phase2 = true;
+                    }
                 }
-                npc.rotation = rotation;
-                MoveTwards(npc, target, 5f, 0f);
-                npc.netUpdate = true;
-            } else if(npc.ai[0] > 1000) {
-                npc.ai[0] = 100;
             }
         }
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Iridium
+## A Terraria Mod
